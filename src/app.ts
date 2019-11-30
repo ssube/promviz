2656b3ab13a2da03975cfa671a1930b038bf3a11
ssube <seansube@gmail.com>
2019-11-29 19:15:32 -0600

    visualize some nodes

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { forceSimulation, hierarchy, select } from 'd3';
+import { forceCenter, forceCollide, forceManyBody, forceSimulation, hierarchy, select } from 'd3';
 
 const STATUS_SUCCESS = 0;
 
@@ -13,13 +13,42 @@ function compileGraph(rawData: string) {
   return hierarchy(parsed);
 }
 
+const GRAPH_SIZE = 512;
+const GRAPH_CENTER = 256;
+const NODE_SIZE = 64;
+
 export async function main(args: ReadonlyArray<string>): Promise<number> {
   const rawData = /* readFile */ '';
   const graphData = compileGraph(rawData);
   const nodes = graphData.descendants();
-  const forceGraph = forceSimulation<{}>(nodes);
-  const svg = select('svg');
-  svg.data(nodes);
+
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  const svg = select('#d3-box')
+    .append('svg')
+    .attr('width', GRAPH_SIZE)
+    .attr('height', GRAPH_SIZE);
+
+  const forceGraph = forceSimulation<{}>(nodes)
+    .force('charge', forceManyBody())
+    .force('center', forceCenter(GRAPH_SIZE / 2, GRAPH_SIZE / 2))
+    .force('collision', forceCollide().radius((d: any) => d.radius))
+    .on('tick', () => {
+      const u = svg.selectAll('g').data(nodes);
+      const e = u.enter()
+        .append('g')
+        .merge(u as any)
+        .attr('transform', (d: any) => `translate(${d.x}, ${d.y})`);
+
+      e.append('circle')
+        .attr('r', NODE_SIZE)
+        .style('fill', 'gray')
+        .style('stroke', 'white');
+
+      e.append('text')
+        .text((d: any) => d.data.name);
+
+      u.exit().remove();
+    });
 
   /* eslint-disable-next-line no-console */
   console.log(forceGraph);
